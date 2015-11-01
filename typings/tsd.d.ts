bfd21b14a92c7cae3508f702d5ae101c7c80ffb2
Josh Milo <josh@joshmilo.com>
2015-10-31 18:59:58 -0700

    clearing old angular2 stuff

--- typings/tsd.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="../typings/angular2/angular2.d.ts" />
-/// <reference path="../typings/es6-promise/es6-promise.d.ts" />
-/// <reference path="../typings/rx/rx.d.ts" />
-/// <reference path="../typings/whatwg-fetch/whatwg-fetch.d.ts" />
-/// <reference path="../typings/main/main.d.ts" />
