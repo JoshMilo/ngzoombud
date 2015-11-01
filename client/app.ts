d5ab92003a3032999a2f6cca337005cc8738d70c
Josh Milo <josh@joshmilo.com>
2015-10-31 21:30:16 -0700

    adding server and collection

--- client/app.ts
+++ client/app.ts
@@ -1,35 +1,21 @@
 /// <reference path="../typings/angular2-meteor.d.ts" />
 
-import { Component, View, NgFor, bootstrap } from 'angular2/angular2';
+import {Component, View, NgFor} from 'angular2/angular2';
+import {Parties} from 'collections/parties';
+import {bootstrap} from 'angular2-meteor';
 
 @Component({
     selector: 'app'
 })
 @View({
-    templateUrl: 'client/app.html',
+    templateUrl: 'client/app.ng.html',
     directives: [NgFor]
 })
 class Socially {
-    parties:Array<Object>;
+    parties: Mongo.Cursor;
 
-    constructor() {
-        this.parties = [
-            {
-                'name': 'Dubstep-Free Zone',
-                'description': 'Can we please just for an evening not listen to dubstep.',
-                'location': 'Palo Alto'
-            },
-            {
-                'name': 'All dubstep all the time',
-                'description': 'Get it on!',
-                'location': 'Palo Alto'
-            },
-            {
-                'name': 'Savage lounging',
-                'description': 'Leisure suit required. And only fiercest manners.',
-                'location': 'San Francisco'
-            }
-        ];
+    constructor () {
+        this.parties = Parties.find();
     }
 }
 
